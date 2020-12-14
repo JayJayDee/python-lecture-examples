35c12de50e033021292fca806561d3fd4e61adc7
JayJayDee <jindongp@gmail.com>
2020-12-14 12:27:17 +0900

    changed score

--- 004/01_the_wizard_q01.py
+++ 004/01_the_wizard_q01.py
@@ -5,7 +5,7 @@ game_result = [
     { 'round': 1, 'name': 'CO', 'score': 20 },
     { 'round': 1, 'name': 'LS', 'score': 10 },
     { 'round': 2, 'name': 'JD', 'score': 20 },
-    { 'round': 2, 'name': 'YE', 'score': 20 },
+    { 'round': 2, 'name': 'YE', 'score': 30 },
     { 'round': 2, 'name': 'HW', 'score': 10 },
     { 'round': 2, 'name': 'CO', 'score': 5 },
     { 'round': 2, 'name': 'LS', 'score': 0 },
@@ -16,7 +16,7 @@ game_result = [
     { 'round': 3, 'name': 'LS', 'score': 0 },
     { 'round': 4, 'name': 'JD', 'score': -10 },
     { 'round': 4, 'name': 'YE', 'score': 20 },
-    { 'round': 4, 'name': 'HW', 'score': 5 },
+    { 'round': 4, 'name': 'HW', 'score': -10 },
     { 'round': 4, 'name': 'CO', 'score': 10 },
     { 'round': 4, 'name': 'LS', 'score': 10 }
 ]
